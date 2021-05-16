d1f68e0a8ac5f7e36c25f81e1eca3dd308f63670
Megakoff <58061450+Megakoff@users.noreply.github.com>
2021-05-15 20:37:11 -0500

    code without test in it

--- table.py
+++ table.py
@@ -365,37 +365,3 @@ class Table(tk.PanedWindow):
         self.update_idletasks()
         self.update()
         print(type(widget))
-
-
-"""
-root = tk.Tk()
-
-table = Table()
-table.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
-
-lista_1 = list(range(100))
-col1 = TableColumn(
-    table,
-    width=15,
-    title='titulo',
-    row_type='label',
-    rows=lista_1
-)
-col1.add_callback(callback=[lambda *x: print('callback_1')])
-
-lista_2 = list(range(100))
-col2 = TableColumn(
-    table,
-    width=10,
-    title='titulo_2',
-    row_type='entry',
-    rows=lista_2,
-    callback=[lambda *x: print('callback')]
-)
-
-table.add_columns([col1, col2])
-
-but = tk.Button(root, text='boton', command=table.ok_command)
-but.pack(side=tk.TOP)
-root.mainloop()
-"""
